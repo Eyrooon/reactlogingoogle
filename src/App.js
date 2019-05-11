69563320fa6de742c474d82f085a8d8e0fa8025e
ivan <ivanaaron.esguerra@gmail.com>
2019-05-12 02:14:47 +0800

    edit readme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { GoogleLogin } from 'react-google-login';
-
-
-// const GoogleButton = () => (
-//   <GoogleLogin
-//     clientId="491004959702-3bgqo54pt777f77dgl7cqd6s7e7rii81.apps.googleusercontent.com"
-//     buttonText="Login"
-//     onSuccess={responseGoogle}
-//     onFailure={responseGoogle}
-//     cookiePolicy={'single_host_origin'}
-//   />
-// )
 
 
 class App extends React.PureComponent {
@@ -28,7 +16,6 @@ class App extends React.PureComponent {
   }
 
   responseGoogle(response) {
-    console.log(response.profileObj);
     const res = response.profileObj;
     this.setState({
       imgSrc: res.imageUrl,
@@ -63,25 +50,5 @@ class App extends React.PureComponent {
     );
   }
 }
-// function App() {
-//   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-//   );
-// }
 
 export default App;
